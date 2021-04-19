7b61c15470ad77bf810af01cf94a05ba92d5a28b
psy-a <psyfi488@gmail.com>
2021-04-19 03:18:10 +0300

    Fix font sizes and format code.

--- js/script.js
+++ js/script.js
@@ -1,91 +1,106 @@
-$(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
-    });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
-    });
+$(document).ready(function () {
+  $("#design-image").click(function () {
+    $("#design-image").slideDown("1500").hide("1000");
+    $("#design").show("1500");
   });
-  $(document).ready(function(){
-    $("#dev-image").click(function(){
-      $("#dev-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
-    });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#dev-image").slideDown('1500');
-    });
+  $("#design").click(function () {
+    $("#design").slideUp("1500");
+    $("#design-image").slideDown("1500");
   });
-  $(document).ready(function(){
-    $("#prod-image").click(function(){
-      $("#prod-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#prod-image").slideDown('1500');
-    });
+});
+$(document).ready(function () {
+  $("#dev-image").click(function () {
+    $("#dev-image").slideDown("1500").hide("1000");
+    $("#development").show("1500");
   });
-  
-  
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
+  $("#development").click(function () {
+    $("#development").slideUp("1500");
+    $("#dev-image").slideDown("1500");
+  });
+});
+$(document).ready(function () {
+  $("#prod-image").click(function () {
+    $("#prod-image").slideDown("1500").hide("1000");
+    $("#product").show("1500");
+  });
+  $("#product").click(function () {
+    $("#product").slideUp("1500");
+    $("#prod-image").slideDown("1500");
+  });
+});
+
+$(document).ready(function () {
+  $("#work1")
+    .mouseover(function () {
       $("#overlay1").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay1").hide();
     });
-    $("#work2").mouseover(function(){
+  $("#work2")
+    .mouseover(function () {
       $("#overlay2").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay2").hide();
     });
-    $("#work3").mouseover(function(){
+  $("#work3")
+    .mouseover(function () {
       $("#overlay3").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay3").hide();
     });
-    $("#work4").mouseover(function(){
+  $("#work4")
+    .mouseover(function () {
       $("#overlay4").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay4").hide();
     });
-    $("#work5").mouseover(function(){
+  $("#work5")
+    .mouseover(function () {
       $("#overlay5").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay5").hide();
     });
-    $("#work6").mouseover(function(){
+  $("#work6")
+    .mouseover(function () {
       $("#overlay6").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay6").hide();
     });
-    $("#work7").mouseover(function(){
+  $("#work7")
+    .mouseover(function () {
       $("#overlay7").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay7").hide();
     });
-    $("#work8").mouseover(function(){
+  $("#work8")
+    .mouseover(function () {
       $("#overlay8").show();
-    }).mouseout(function(){
+    })
+    .mouseout(function () {
       $("#overlay8").hide();
     });
+});
+
+$(document).ready(function () {
+  $("form#mc-form").submit(function (event) {
+    // event.preventDefault();
+    var name = $("input#NAME").val();
+    var email = $("input#EMAIL").val();
+    var message = $("textarea#MESSAGE").val();
+    if (name && email) {
+      alert(
+        name +
+          ", we have received your message, thank you for reaching out to us."
+      );
+    } else {
+      alert("Please enter your name and Email.");
+    }
   });
-  
-  $(document).ready(function(){
-    $("form#mc-form").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#NAME").val();
-      var email = $("input#EMAIL").val();
-      var message = $("textarea#MESSAGE").val();
-      if (name && email){
-        alert (name + ", we have received your message, thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and Email.");
-      }
-      
-    });
-  
-  });
+});
